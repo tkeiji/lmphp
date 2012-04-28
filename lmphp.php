4c833b595006802aba74259e1e9183cb5613f1e2
Keiji Takeda <tkeiji2@gmail.com>
2012-04-28 19:36:11 +0900

    update incomplete

--- lmphp.php
+++ lmphp.php
@@ -1,22 +1,20 @@
 <?php
-	$ctime = date("G:i");
+	$today = new DateTime();
 	print('Current Time = ');
-    print( $ctime );	
+    print( $today->format('Y/m/d H:i:s') );	
 	print('<br>');
-	
-    print('--reg bus--');
+	$bustime = $today;
+    print('--reg bus--<br>');
 	$fp = fopen("regbustable.txt", "r");
     while ( !feof($fp) ){
     	$line = fgets($fp);
     	$stime = split(" ", $line);
     	foreach( $stime as $eachtime){
     		$stime = split(":", $eachtime);
-    		 
-    		
-    		
-    		
-    		print ( $eachtime );
+    		print("Hour = $stime[0], Min = $stime[1]"); 
     		print ( "<br>" );
+    		$bustime->setTime($stime[0], $stime[1], 0);
+    		print("bustime= $bustime->format('H:i:s')<br>");
     	}
     }
 
